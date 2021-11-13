f55db09f335ad3ebd15a69893d39bcd6a232788c
Chao Chen <chaochen@live.com>
2021-11-13 15:18:51 -0500

    Create - connection to MongoDB

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ require('express-async-errors');
 const express = require('express');
 const app = express();
 
+// connectDB
+const connectDB = require('./db/connect');
+
 // Routes
 app.get('/', (req, res) => {
   res.send('Employee Directory API');
@@ -13,6 +16,7 @@ const port = process.env.PORT || 5000;
 
 const start = async () => {
   try {
+    await connectDB(process.env.MONGO_URI);
     app.listen(port, () => console.log(`Server is listening on port ${port}...`));
   } catch (error) {
     console.log(error);
