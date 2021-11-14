5f3f8448572b02e8fe0b56f36636913a8a8fd792
Chao Chen <chaochen@live.com>
2021-11-13 19:52:30 -0500

    Add - xss-clean package to sanitize user inputs and add extra security

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ require('express-async-errors');
 // Security
 const helmet = require('helmet');
 const cors = require('cors');
+const xss = require('xss-clean');
 
 const express = require('express');
 const app = express();
@@ -21,6 +22,7 @@ const errorHandlerMiddleware = require('./middleware/error-handler');
 app.use(express.json());
 app.use(helmet());
 app.use(cors());
+app.use(xss());
 
 // Routes
 app.get('/', (req, res) => {
