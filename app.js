083e039ac7f27c83e89dbacac00e029a2921fdf5
Chao Chen <chaochen@live.com>
2021-11-13 20:54:08 -0500

    Add - swagger docs

--- app.js
+++ app.js
@@ -7,6 +7,11 @@ const cors = require('cors');
 const xss = require('xss-clean');
 const rateLimiter = require('express-rate-limit');
 
+// Swagger
+const swaggerUI = require('swagger-ui-express');
+const YAML = require('yamljs');
+const swaggerDocument = YAML.load('./swagger.yaml');
+
 const express = require('express');
 const app = express();
 
@@ -34,8 +39,9 @@ app.use(xss());
 
 // Routes
 app.get('/', (req, res) => {
-  res.send('Employee Directory API');
+  res.send('<h1>Employees API</h1><a href="/api-docs">Documentation</a>');
 });
+app.use('/api-docs', swaggerUI.serve, swaggerUI.setup(swaggerDocument));
 app.use('/api/v1/employees', employeeRouter);
 
 app.use(notFoundMiddleware);
