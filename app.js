f27d79539f5080bfdd655acabbb072cebaba3384
Chao Chen <chaochen@live.com>
2021-11-13 20:06:54 -0500

    Add - rate limits

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ require('express-async-errors');
 const helmet = require('helmet');
 const cors = require('cors');
 const xss = require('xss-clean');
+const rateLimiter = require('express-rate-limit');
 
 const express = require('express');
 const app = express();
@@ -19,6 +20,13 @@ const employeeRouter = require('./routes/employee');
 const notFoundMiddleware = require('./middleware/not-found');
 const errorHandlerMiddleware = require('./middleware/error-handler');
 
+app.set('trust proxy', 1);
+app.use(
+  rateLimiter({
+    windowMs: 15 * 60 * 1000,
+    max: 100,
+  })
+);
 app.use(express.json());
 app.use(helmet());
 app.use(cors());
